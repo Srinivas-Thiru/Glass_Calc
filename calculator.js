4174a0493028ddd5a0b8803738ef0bee14a50d4d
Srinivas T <110653801+Srinivas-Thiru@users.noreply.github.com>
2023-08-04 20:14:42 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -11,7 +11,7 @@ document.querySelectorAll("input")[i].addEventListener("click", display);
 function display() {
    var val = this.valueOf().value;
    if(val === "=") {
-      document.querySelector(".display").valueOf().value = eval(document.querySelector(".display").valueOf().value);
+      document.querySelector(".display").valueOf().value = parseFloat(eval(document.querySelector(".display").valueOf().value)).toFixed(4);
    }else if(val === "AC"){
       document.querySelector(".display").valueOf().value = "";
    }else if(val === "DEL"){
